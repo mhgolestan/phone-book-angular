427c59962af26c69df332997e45b8f40090c4c42
mhgolestan <golestan1369@gmail.com>
2022-05-21 17:15:13 +0200

    HttpClient used for GET

--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { PERSONS } from './mock-persons';
 import { Person } from './person';
 import { MessageService } from './message.service';
-
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
+  private personsUrl = 'api/persons';  // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient,private messageService: MessageService) { }
 
   getPersons(): Observable<Person[]> {
-    const persons = of(PERSONS);
-    this.messageService.add('PersonService: fetched persons');
-    return persons;
+    return this.http.get<Person[]>(this.personsUrl);
   }
 
   getPerson(id: number): Observable<Person> {
@@ -23,6 +23,11 @@ export class PersonService {
     const person = PERSONS.find(p => p.id === id)!;
     this.messageService.add(`PersonService: fetched person id=${id}`);
     return of(person);
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`PersonService: ${message}`);
   }
 
 }
