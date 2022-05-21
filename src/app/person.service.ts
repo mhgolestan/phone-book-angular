8e46f22faba5940edcfbbc02667ecd644491203c
mhgolestan <golestan1369@gmail.com>
2022-05-21 17:23:40 +0200

    error handling for person service implemented

--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { PERSONS } from './mock-persons';
 import { Person } from './person';
 import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +16,11 @@ export class PersonService {
   constructor(private http: HttpClient,private messageService: MessageService) { }
 
   getPersons(): Observable<Person[]> {
-    return this.http.get<Person[]>(this.personsUrl);
+    return this.http.get<Person[]>(this.personsUrl)
+          .pipe(
+            tap(_ => this.log('fetched persons')),
+            catchError(this.handleError<Person[]>('getPerson', []))
+          );
   }
 
   getPerson(id: number): Observable<Person> {
@@ -28,6 +34,27 @@ export class PersonService {
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`PersonService: ${message}`);
+  }
+
+    /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
